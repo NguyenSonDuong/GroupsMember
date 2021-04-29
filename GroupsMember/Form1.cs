b355d496559f08199da2bef9c54f35f3920412b2
NguyenSonDuong <hunterdemon9x99@gmai.com>
2021-04-29 16:27:24 +0700

    Get Member Groups

---
+++ GroupsMember/Form1.cs
@@ -0,0 +1,107 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FacebookAPI.model;
+using System.Threading;
+
+namespace GroupsMember
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void AddCookie(String cookie, RestRequest request)
+        {
+            String[] cookies = cookie.Split(';');
+            foreach(String item in cookies)
+            {
+                String[] cookieItem = item.Trim().Split('=');
+                if (cookieItem.Length > 1)
+                {
+                    request.AddCookie(cookieItem[0], cookieItem[1]);
+                }
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Thread trh = new Thread(() =>
+            {
+                GetMember("");
+            });
+            trh.IsBackground = true;
+            trh.Start();
+        }
+        int dem = 0;
+        public void GetMember(String cursor)
+        {
+            var client = new RestClient("https://www.facebook.com/api/graphql");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.POST);
+            AddCookie("fr=1kBct58bPO5na4geO.AWXD_7Q_KFRGIXcv4nBOIXzweck.Bgh5e8.uR.AAA.0.0.BgiPmQ.AWVbTkshOds; sb=vJeHYJasHf3bKiNa5DCh40MF; wd=1903x966; datr=vJeHYMGy3juy1W3v9ufNRonU; _fbp=fb.1.1619588906476.1215958157; locale=vi_VN; c_user=100010625562840; xs=20%3Ane5vEjc3l4F7YA%3A2%3A1619589521%3A9766%3A2631; spin=r.1003705778_b.trunk_t.1619681397_s.1_v.2_", request);
+            client.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36";
+            request.AddHeader("Accept", "*/*");
+            request.AddHeader("Referer", "https://www.facebook.com/groups/2908365305855309/members");
+            request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+            request.AddParameter("av", "100010625562840");
+            request.AddParameter("__user", "100010625562840");
+            request.AddParameter("__a", "1");
+            request.AddParameter("dpr", "1");
+            request.AddParameter("fb_dtsg", "AQGwGbZr7Qf2ruY:AQEG8cYMs5NaEO8");
+            request.AddParameter("jazoest", "22456");
+            request.AddParameter("fb_api_caller_class", "RelayModern");
+            request.AddParameter("fb_api_req_friendly_name", "useGroupsCometMemberSearchResultsRefetchQuery");
+            String vari = "{\"count\":20,\"cursor\":\"" + cursor + "\",\"groupID\":\"364997627165697\",\"query\":\"\",\"scale\":1,\"id\":\"364997627165697\"}";
+            request.AddParameter("variables", vari);
+            request.AddParameter("server_timestamps", "true");
+            request.AddParameter("doc_id", "4391041100924633");
+            IRestResponse response = client.Execute(request);
+            if(response != null && !String.IsNullOrEmpty(response.Content))
+            {
+                FacebookAPI.model.GroupsMember groupsMember = JsonConvert.DeserializeObject<FacebookAPI.model.GroupsMember>(response.Content);
+                if(groupsMember.data.node.search_results.page_info != null && groupsMember.data.node.search_results.page_info.has_next_page)
+                {
+                    
+                    foreach(Edge item in groupsMember.data.node.search_results.edges)
+                    {
+                        dem++;
+                        richTextBox1.Invoke(new MethodInvoker(() =>
+                        {
+                            richTextBox1.AppendText(dem+" : "+item.node.id + " | " + item.node.name + "\n");
+                            richTextBox1.ScrollToCaret();
+                        }));
+                    }
+                    Thread.Sleep(1000);
+                    GetMember(groupsMember.data.node.search_results.page_info.end_cursor);
+                }
+                else
+                {
+                    richTextBox1.Invoke(new MethodInvoker(() =>
+                    {
+                        richTextBox1.SelectionColor = Color.Green;
+                        richTextBox1.AppendText("Đã hết\n");
+                        richTextBox1.ScrollToCaret();
+                    }));
+                }
+            }
+            else
+            {
+                richTextBox1.Invoke(new MethodInvoker(() =>
+                {
+                    richTextBox1.SelectionColor = Color.Red;
+                    richTextBox1.AppendText("LỖI-----\n");
+                    richTextBox1.ScrollToCaret();
+                }));
+            }
+        }
+    }
+}
---
+++ GroupsMember/GroupsMember.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FacebookAPI.model
+{
+    public class GroupsMember
+    {
+        public Data data { get; set; }
+    }
+
+
+    public class Data
+    {
+        public Node node { get; set; }
+    }
+
+    public class Node
+    {
+        public string __typename { get; set; }
+        public string id { get; set; }
+        public Search_Results search_results { get; set; }
+    }
+
+    public class Search_Results
+    {
+        public Edge[] edges { get; set; }
+        public Page_Info page_info { get; set; }
+    }
+
+    public class Page_Info
+    {
+        public string end_cursor { get; set; }
+        public bool has_next_page { get; set; }
+    }
+
+    public class Edge
+    {
+        public Join_Status_Text join_status_text { get; set; }
+        public Node1 node { get; set; }
+        public string cursor { get; set; }
+    }
+
+    public class Join_Status_Text
+    {
+        public object[] delight_ranges { get; set; }
+        public object[] image_ranges { get; set; }
+        public object[] inline_style_ranges { get; set; }
+        public object[] aggregated_ranges { get; set; }
+        public object[] ranges { get; set; }
+        public object[] color_ranges { get; set; }
+        public string text { get; set; }
+    }
+
+    public class Node1
+    {
+        public string __typename { get; set; }
+        public string id { get; set; }
+        public string __isProfile { get; set; }
+        public Group_Membership group_membership { get; set; }
+        public string name { get; set; }
+        public bool is_verified { get; set; }
+        public string __isEntity { get; set; }
+        public object work_foreign_entity_info { get; set; }
+        public string url { get; set; }
+        public string __isGroupMember { get; set; }
+        public Profile_Picture profile_picture { get; set; }
+        public Bio_Text bio_text { get; set; }
+        public User_Type_Renderer user_type_renderer { get; set; }
+    }
+
+    public class Group_Membership
+    {
+        public User_Signals_Info user_signals_info { get; set; }
+        public string id { get; set; }
+        public bool has_member_feed { get; set; }
+        public Associated_Group associated_group { get; set; }
+        public object[] member_actions { get; set; }
+        public Action_Types[] action_types { get; set; }
+    }
+
+    public class User_Signals_Info
+    {
+        public object[] displayed_user_signals { get; set; }
+        public bool has_more { get; set; }
+        public string overflow_uri { get; set; }
+        public int total_count { get; set; }
+    }
+
+    public class Associated_Group
+    {
+        public string id { get; set; }
+        public Leaders_Engagement_Logging_Settings leaders_engagement_logging_settings { get; set; }
+    }
+
+    public class Leaders_Engagement_Logging_Settings
+    {
+        public Comet_Surface_Mappings[] comet_surface_mappings { get; set; }
+    }
+
+    public class Comet_Surface_Mappings
+    {
+        public string __typename { get; set; }
+        public string surface { get; set; }
+        public string trace_policy { get; set; }
+        public string[] prefixes { get; set; }
+    }
+
+    public class Action_Types
+    {
+        public string __typename { get; set; }
+        public string action_type { get; set; }
+    }
+
+    public class Profile_Picture
+    {
+        public string uri { get; set; }
+        public int width { get; set; }
+        public int height { get; set; }
+        public int scale { get; set; }
+    }
+
+    public class Bio_Text
+    {
+        public object[] delight_ranges { get; set; }
+        public object[] image_ranges { get; set; }
+        public object[] inline_style_ranges { get; set; }
+        public object[] aggregated_ranges { get; set; }
+        public object[] ranges { get; set; }
+        public object[] color_ranges { get; set; }
+        public string text { get; set; }
+    }
+
+    public class User_Type_Renderer
+    {
+        public string __typename { get; set; }
+        public User user { get; set; }
+        public __Module_Operation_Groupscometmemberspagememberrowdefaultprofileaddon_Profile __module_operation_GroupsCometMembersPageMemberRowDefaultProfileAddOn_profile { get; set; }
+        public __Module_Component_Groupscometmemberspagememberrowdefaultprofileaddon_Profile __module_component_GroupsCometMembersPageMemberRowDefaultProfileAddOn_profile { get; set; }
+    }
+
+    public class User
+    {
+        public string __typename { get; set; }
+        public string id { get; set; }
+        public string friendship_status { get; set; }
+    }
+
+    public class __Module_Operation_Groupscometmemberspagememberrowdefaultprofileaddon_Profile
+    {
+        public string __dr { get; set; }
+    }
+
+    public class __Module_Component_Groupscometmemberspagememberrowdefaultprofileaddon_Profile
+    {
+        public string __dr { get; set; }
+    }
+
+    
+}
